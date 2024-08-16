0d7a07b7e6ee76c67deb42f0d6184106d350c4e0
Jay <original.jaysharma@gmail.com>
2024-08-16 14:24:28 +0530

    promises

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import app from "./app.js";
 
 dotenv.config({path: "./env"});
 
-connectDB();
+connectDB()
+.then()
+.catch((err) => {
+    console.log("mongo db connection got 20/100 in test and failed",err);
+})
