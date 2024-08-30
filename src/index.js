1d0c62f152571b2bdbf3391729d31f6a356ac995
Jay <original.jaysharma@gmail.com>
2024-08-30 21:03:15 +0530

    no updates

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import app from "./app.js";
 dotenv.config({path: "./env"});
 
 connectDB()
-.then()
+.then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log("server started at port",process.env.PORT);
+    })
+})
 .catch((err) => {
     console.log("mongo db connection got 20/100 in test and failed",err);
 })
